8fd436cc9725e493e1c2159409f740eaf58005d2
Graham Duthie <grahamd483@gmail.com>
2023-09-28 18:14:43 +0200

    Initial set up -> used recommended lower levels (no, no) instead of (yes, yes) handles up to 32.

---
+++ desktop/src/com/obstacle/avoid/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.obstacle.avoid;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.obstacle.avoid.ObstacleAvoidGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("obstacle-avoid-ashley");
+		new Lwjgl3Application(new ObstacleAvoidGame(), config);
+	}
+}
