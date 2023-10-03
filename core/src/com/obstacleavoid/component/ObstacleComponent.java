eab5c89e9aa2a2029352aa07740f45dedbd58278
Graham Duthie <grahamd483@gmail.com>
2023-10-03 09:10:48 +0200

    142c. Poolable implemented in component and reset method called automatically.

--- core/src/com/obstacleavoid/component/ObstacleComponent.java
+++ core/src/com/obstacleavoid/component/ObstacleComponent.java
@@ -1,8 +1,15 @@
 package com.obstacleavoid.component;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool.Poolable;
 
-public class ObstacleComponent implements Component
+public class ObstacleComponent implements Component, Poolable
 {
     public boolean hitAlready;
+
+    @Override
+    public void reset( )
+    {
+        hitAlready = false;
+    }
 }
