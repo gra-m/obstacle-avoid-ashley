20bbc19e34da8f40aa9b2b4399550879750145d0
Graham Duthie <grahamd483@gmail.com>
2023-10-04 10:36:00 +0200

    147e. WorldWrapSystem not linked specifically to player, DimensionComponent added to family.

--- core/src/com/obstacleavoid/system/debug/WorldWrapSystem.java
+++ core/src/com/obstacleavoid/system/debug/WorldWrapSystem.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.utils.Logger;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.obstacleavoid.common.GameManager;
 import com.obstacleavoid.common.Mappers;
+import com.obstacleavoid.component.DimensionComponent;
 import com.obstacleavoid.component.PositionComponent;
 import com.obstacleavoid.component.WorldWrapComponent;
 import com.obstacleavoid.config.GameConfig;
@@ -17,7 +18,8 @@ public class WorldWrapSystem extends IteratingSystem
     private static final Logger LOG = new Logger(WorldWrapSystem.class.getName(), Logger.DEBUG);
     private static final Family FAMILY = Family.all(
             WorldWrapComponent.class,
-            PositionComponent.class
+            PositionComponent.class,
+            DimensionComponent.class
     ).get();
     private final Viewport viewport;
     
@@ -31,12 +33,14 @@ public class WorldWrapSystem extends IteratingSystem
     protected void processEntity( Entity entity, float deltaTime )
     {
         PositionComponent positionComponent = Mappers.POSITION_COMPONENT_MAPPER.get(entity);
+        DimensionComponent dimensionComponent = Mappers.DIMENSION_COMPONENT_MAPPER.get(entity);
 
         // clamp positions:
         positionComponent.x = MathUtils.clamp(positionComponent.x, 0,
-                viewport.getWorldWidth() - GameConfig.PLAYER_SIZE);
-        positionComponent.y = MathUtils.clamp(positionComponent.y, 0 + GameConfig.PLAYER_BOUNDS_RADIUS,
-                viewport.getWorldHeight() - GameConfig.PLAYER_BOUNDS_RADIUS) ;
+                viewport.getWorldWidth() - dimensionComponent.width);
+        // add up and down to listener to enable this.
+        positionComponent.y = MathUtils.clamp(positionComponent.y, 0 + dimensionComponent.height/2,
+                viewport.getWorldHeight() - dimensionComponent.height) ;
 
     }
 }
